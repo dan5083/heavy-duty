ce68b21af801615223e1e5740522e97526462d21
Dan Bayliss <dan5083@outlook.com>
2025-06-19 21:58:52 +0100

    Add suggestion for 8-day split and 'choose wisely' header
    - Mark 8-day split as suggested with green text
    - Add encouraging header text to split selection

--- app/controllers/training_archive_controller.rb
+++ app/controllers/training_archive_controller.rb
@@ -1,6 +1,17 @@
 class TrainingArchiveController < ApplicationController
   def index
-    @muscles = AppConstants::LABELS
+    # Get only muscle groups that the current user actually has workouts for
+    user_muscle_groups = current_user.workout_logs
+                                    .joins(:workout)
+                                    .distinct
+                                    .pluck('workouts.muscle_group')
+                                    .map(&:to_sym)
+
+    # Filter LABELS to only include muscle groups the user has (exclude split plan labels)
+    @muscles = AppConstants::LABELS.select do |key, _label|
+      user_muscle_groups.include?(key) && AppConstants::WORKOUTS.key?(key)
+    end
+
     @selected_muscle = params[:muscle]
 
     @logs = current_user.workout_logs.includes(:workout).order(created_at: :desc)
