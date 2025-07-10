1f8af198743f462039bd8a2f17c2ba1a5e2c0663
Dan Bayliss <dan5083@outlook.com>
2025-07-10 12:58:56 +0100

    Remove outdated test suite

--- test/controllers/training_archive_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class TrainingArchiveControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get training_archive_index_url
-    assert_response :success
-  end
-end
--- test/models/personal_trainer_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class PersonalTrainerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
